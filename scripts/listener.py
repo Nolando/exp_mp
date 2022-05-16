2fd843570d9a5d0d449394a352905df9a495b9cc
Antony Wu <antony6889@gmail.com>
2022-05-16 14:45:33 +1000

    Added colour threshold for red shirt

--- scripts/listener.py
+++ scripts/listener.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
+# Machine Unlearning
+
 import rospy
-from std_msgs.msg import String
+import cv2 as cv
 import numpy as np
-
+from std_msgs.msg import String
 from exp_mp.msg import bounding_box
-
-import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
 # Initialize the CvBridge class
 bridge = CvBridge()    
 
+# Set the limits for the colours (in HSV)
+red_low_limit = np.array([140, 30, 0])          # Red
+red_high_limit = np.array([179, 255, 255])
+# green_low_limit = np.array([140, 30, 0])          # Green
+# green_high_limit = np.array([179, 255, 255])
+
+
+#################################################################################
 # Define a function to show the image in an OpenCV Window
 def show_image(img):
-    cv2.imshow("Image Window", img)
-    cv2.waitKey(3)
 
+    # Flip camera feed for correct orientation
+    flipped_img = cv.flip(img, -1)
+
+    # Display the image
+    cv.imshow("Image Window", flipped_img)
+    cv.waitKey(3)
+
+#################################################################################
 def image_callback(img_msg):
     # log some info about the image topic
     rospy.loginfo("Image received")
@@ -30,12 +44,44 @@ def image_callback(img_msg):
     # Show the converted image
     # show_image(cv_image)
 
+#################################################################################
+# Script segments the colours based on t shirt colour: using RED and GREEN
+def colour_segmentaion(img):
+
+    # Convert RGB into HSV
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    # Threshold the image for red (HSV colour space)
+    thresh = cv.inRange(hsv, red_low_limit, red_high_limit)
+
+    # Morphological open to help detections, kernel size 10x10 pixels
+    kernel = np.ones((10, 10), np.uint8)
+    img_bw = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
+
+    # Display the red shirt detection
+    # view_image("Red Shirt Detection", img_bw)
+
+    # Find the valid contours in the bw image for the regions detected
+    contours = cv.findContours(img_bw, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+     # Loop through the detected contours
+    for c in contours:
+
+        # Calculate the area of the region
+        area = cv.contourArea(c)
+        print(area)
+    
+    
+
+#################################################################################
 def bound_callback(box):
 
     rospy.loginfo("Bounding box received")
 
     print(box)
 
+#################################################################################
 def listener():
 
     print('Initiating listener')
